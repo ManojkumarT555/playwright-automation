90d35e86a107afac9d148e0ea98394ff58149120
ManojkumarT555 <manojkumart2004@gmail.com>
2026-04-14 12:16:08 +0530

    Added login automation script

--- tests/navigations.spec.ts
+++ tests/navigations.spec.ts
@@ -1,11 +1,17 @@
-import {test,expect} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('navigating to website', async({page})=>{
+test('login test', async ({ page }) => {
+
+  //  test.setTimeout(60000); // increase timeout
   await page.goto("https://www.saucedemo.com/");
-  await expect(page).toHaveTitle(/Swag Labs/); 
-  await page.getByPlaceholder('Username').pressSequentially("standard_user");
-  await page.getByPlaceholder('Password').pressSequentially("secret_sauce");
-  await page.locator("#login-button").click();
+
+  await expect(page).toHaveTitle(/Swag Labs/);
+
+  await page.getByPlaceholder('Username').fill("standard_user");
+  await page.getByPlaceholder('Password').fill("secret_sauce");
+
+  await page.getByRole('button', { name: 'Login' }).click();
+
   await expect(page).toHaveURL(/inventory/);
-  
-})
+  await expect(page.locator('.inventory_list')).toBeVisible();
+});
