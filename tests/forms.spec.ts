60503b229d6e99986e5fa00a6c8e563ea2c076a3
ManojkumarT555 <manojkumart2004@gmail.com>
2026-04-15 23:28:04 +0530

    Checkbox,dropdown automation script written

--- tests/forms.spec.ts
+++ tests/forms.spec.ts
@@ -11,14 +11,32 @@ test("Automating the Forms", async({page})=>{
     
     await page.locator("#textarea").fill("No.10, Sajinbin Street, Chennai - 600008, TamilNadu");
 
+})
+
+test("Radio and Checkboxes",async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/");
+
+    await expect(page).toHaveURL(/testautomationpractice/);
     //Checking the radio buttons
     await expect(page.getByText("Gender:")).toBeVisible();
     await page.locator("#male").check();
     await expect(page.locator("#male")).toBeChecked();
 
     //Checking the Checkboxes
-    // await expect(page.getByText("Days:")).toBeVisible();
-    // await page.locator("[]").check();
+    const days=["#monday","#tuesday","#wednesday","#thursday","#friday"];
+    for (const day of days){
+        await page.locator(day).check();
+    }
+})
 
+test("Dropdown and dynamic dropdowns",async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/");
 
+    await expect(page).toHaveURL(/testautomationpractice/);
+    //Handling dropdowns
+    await expect(page.getByText("Country:")).toBeVisible();
+    await page.locator("#country").selectOption("india");
+
+    await expect(page.getByText("Colors:")).toBeVisible();
+    await page.locator("#colors").selectOption(["red","blue","green"]);
 })
